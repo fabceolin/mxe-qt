aafaa7278484b87783feb7be7922fff11eeae7bc
Fabrício Ceolin <fabricio.ceolin@miningmath.com>
2021-07-19 18:37:06 -0300

    add curl package

---
+++ src/curl.mk
@@ -0,0 +1,38 @@
+# This file is part of MXE. See LICENSE.md for licensing information.
+
+PKG             := curl
+$(PKG)_WEBSITE  := https://curl.haxx.se/libcurl/
+$(PKG)_DESCR    := cURL
+$(PKG)_IGNORE   :=
+$(PKG)_VERSION  := 7.77.0
+$(PKG)_CHECKSUM := 0f64582c54282f31c0de9f0a1a596b182776bd4df9a4c4a2a41bbeb54f62594b
+$(PKG)_SUBDIR   := curl-$($(PKG)_VERSION)
+$(PKG)_FILE     := curl-$($(PKG)_VERSION).tar.xz
+$(PKG)_URL      := https://curl.haxx.se/download/$($(PKG)_FILE)
+$(PKG)_DEPS     := cc libidn2 libssh2 pthreads
+
+define $(PKG)_UPDATE
+    $(WGET) -q -O- 'https://curl.haxx.se/download/?C=M;O=D' | \
+    $(SED) -n 's,.*curl-\([0-9][^"]*\)\.tar.*,\1,p' | \
+    head -1
+endef
+
+define $(PKG)_BUILD
+    cd '$(BUILD_DIR)' && $(SOURCE_DIR)/configure \
+        $(MXE_CONFIGURE_OPTS) \
+        --with-winssl \
+        --without-ssl \
+        --with-libidn2 \
+        --enable-sspi \
+        --enable-ipv6 \
+        --with-libssh2 \
+        LIBS=`'$(TARGET)-pkg-config' pthreads --libs`
+    $(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)' $(MXE_DISABLE_DOCS)
+    $(MAKE) -C '$(BUILD_DIR)' -j 1 install $(MXE_DISABLE_DOCS)
+    ln -sf '$(PREFIX)/$(TARGET)/bin/curl-config' '$(PREFIX)/bin/$(TARGET)-curl-config'
+
+    '$(TARGET)-gcc' \
+        -W -Wall -Werror -ansi -pedantic \
+        '$(TEST_FILE)' -o '$(PREFIX)/$(TARGET)/bin/test-curl.exe' \
+        `'$(TARGET)-pkg-config' libcurl --cflags --libs`
+endef
